f27d40078803ed488960e5eb4e3e16c4ae58afc0
CaramelYo_CGVSL_PC <alltime326@gmail.com>
2018-10-14 20:54:09 +0800

    first commit

---
+++ Pinpon2015/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Pingpon20215
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.board = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.NPC = new System.Windows.Forms.Label();
+            this.lblNotification = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // board
+            // 
+            this.board.BackColor = System.Drawing.Color.Black;
+            this.board.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.board.Location = new System.Drawing.Point(125, 257);
+            this.board.Name = "board";
+            this.board.Size = new System.Drawing.Size(109, 23);
+            this.board.TabIndex = 4;
+            this.board.Text = "YOU";
+            this.board.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.board.MouseDown += new System.Windows.Forms.MouseEventHandler(this.P_MouseDown);
+            this.board.MouseMove += new System.Windows.Forms.MouseEventHandler(this.P_MouseMove);
+            // 
+            // ball
+            // 
+            this.ball.Image = global::Pinpon2015.Properties.Resources.ball;
+            this.ball.Location = new System.Drawing.Point(162, 87);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(32, 32);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ball.TabIndex = 3;
+            this.ball.TabStop = false;
+            // 
+            // NPC
+            // 
+            this.NPC.BackColor = System.Drawing.Color.Black;
+            this.NPC.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.NPC.Location = new System.Drawing.Point(103, 7);
+            this.NPC.Name = "NPC";
+            this.NPC.Size = new System.Drawing.Size(109, 23);
+            this.NPC.TabIndex = 5;
+            this.NPC.Text = "NPC";
+            this.NPC.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNotification
+            // 
+            this.lblNotification.AutoSize = true;
+            this.lblNotification.Location = new System.Drawing.Point(125, 172);
+            this.lblNotification.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNotification.Name = "lblNotification";
+            this.lblNotification.Size = new System.Drawing.Size(101, 12);
+            this.lblNotification.TabIndex = 6;
+            this.lblNotification.Text = "按住滑板開始遊戲";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(343, 313);
+            this.Controls.Add(this.lblNotification);
+            this.Controls.Add(this.NPC);
+            this.Controls.Add(this.board);
+            this.Controls.Add(this.ball);
+            this.Name = "Form1";
+            this.Text = "乒乓球";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label board;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Label NPC;
+        private System.Windows.Forms.Label lblNotification;
+    }
+}
+
---
+++ Pinpon2015/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pingpon20215
+{
+    public partial class Form1 : Form
+    {
+        int[] directions = new int[] { -1, 1 };
+
+        private Random rand;
+
+        int speed_upper_bound = 5, speed_lower_bound = 1;
+        int npc_speed_x = 3;
+
+        int ball_init_space_interval;
+        int ball_vx, ball_vy;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            rand = new Random();
+
+            ball_init_space_interval = -1;
+
+            // top
+            ball_init_space_interval = NPC.Bottom > ball_init_space_interval ? NPC.Bottom : ball_init_space_interval;
+            // bottom
+            ball_init_space_interval = (Width - board.Top) + ball.Height > ball_init_space_interval ? (Width - board.Top) + ball.Height : ball_init_space_interval;
+            // right
+            ball_init_space_interval = ball.Width > ball_init_space_interval ? ball.Width : ball_init_space_interval;
+
+            //MessageBox.Show("ball_init_space_interval = " + ball_init_space_interval.ToString());
+
+            timer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        // 初始化各項變數
+        private void init()
+        {
+            // init the location of ball randomly
+            ball.Top = rand.Next(ball_init_space_interval, Height - ball_init_space_interval);
+            ball.Left = rand.Next(ball_init_space_interval, Width - ball_init_space_interval);
+
+            // init the velocity of ball randomly
+            ball_vx = directions[rand.Next(1)] * rand.Next(speed_lower_bound, speed_upper_bound);
+            ball_vy = directions[rand.Next(1)] * rand.Next(speed_lower_bound, speed_upper_bound);
+
+            lblNotification.Visible = true;
+        }
+
+        private void catch_or_not(Label l, string message)
+        {
+            if (ball.Right >= l.Left && ball.Left <= l.Right) //球在球拍寬度範圍內
+            {
+                // catch
+                //double F = ((double)center - (double)l.Left) / (double)l.Width; // 以擊球點計算擊球力道
+                //if (ball_vx < 0) { F = (1.0 - F); } // 力道方向調整
+                //F = F + 0.5; // 力道大小修正
+
+                double a = (ball.Left - (l.Left - ball.Width)) / (double)(l.Width + 2 * ball.Width); // 以擊球點計算擊球力道
+                a = ball_vx < 0 ? 1.0 - a : a; // 力道方向調整
+                a += 1.5; // 力道大小修正
+
+                // 施加力道
+                ball_vx += (int)Math.Round(a * timer1.Interval);
+                //反彈
+                ball_vy = -ball_vy;
+            }
+            else // 沒接到
+            {
+                // didn't catch
+                // 結束球的運動
+                timer1.Stop();
+
+                //顯示訊息結束遊戲
+                MessageBox.Show(message);
+                
+                // 重新開始
+                Form1_Load(this, null);
+            }
+        }
+
+        //球的移動控制
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*** 處理球的碰撞 ***/
+            //X方向移動
+            //Y方向移動
+            // motion of ball
+            ball.Top += ball_vy * timer1.Interval;
+            ball.Left += ball_vx * timer1.Interval;
+            
+            //處理左邊界碰撞
+            //處理右邊界碰撞
+            // the collision of left and right boundary
+            ball_vx = ball.Left >= 0 && ball.Right < Width ? ball_vx : -ball_vx;
+            
+            if (ball.Top < NPC.Bottom) //高於NPC高度時(可能碰撞板子)
+                catch_or_not(NPC, "You win");
+
+            if (ball.Bottom > board.Top) //低於擊球板高度時(可能碰撞板子)
+                catch_or_not(board, "Game over");
+
+            /*** 處理NPC的移動 ***/
+            int npc_center = (NPC.Right + NPC.Left) / 2;
+            int ball_center = (ball.Left + ball.Right) / 2; //球的中點X座標
+
+            if (ball_center < npc_center) // 球在左邊向左移
+                NPC.Left -= npc_speed_x * timer1.Interval;
+            else if (ball_center > npc_center) // 球在右邊向右移
+                NPC.Left += npc_speed_x * timer1.Interval;
+        }
+
+        int mdx; //球拍拖曳X起點
+
+        //開始拖曳
+        private void P_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(timer1.Enabled == false) // 如果遊戲還沒開始
+            {
+                timer1.Start();
+                lblNotification.Visible = false;
+            }
+
+            //拖曳X起點
+            // the x coordinate in the container of  mouse
+            mdx = e.X;
+        }
+
+        //拖曳中
+        private void P_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) //左鍵按下(拖曳)狀態
+                board.Left += e.X - mdx;
+        }
+    }
+}
